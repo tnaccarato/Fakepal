6136dff902586d0f35e7f42bc73c47e2f85736ea
Tom Naccarato <tcarlnaccarato@gmail.com>
2024-03-01 13:50:23 +0000

    Add account model

--- register/models.py
+++ register/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    """
+    Account model for storing user account information.
+
+    Attributes:
+    - user: OneToOneField to User model
+    - balance: DecimalField to store account balance
+    - CURRENCY_CHOICES: Tuple of tuples to store currency choices
+    - currency: CharField to store currency type
+    - created_at: DateTimeField to store account creation date
+    - STATUS_CHOICES: Tuple of tuples to store account status choices
+    - status: CharField to store account status
+
+    Methods:
+    - __str__: Returns the username of the user linked to the account
+
+    """
+    class Meta:
+        db_table = 'account'
+        verbose_name = 'Account'
+        verbose_name_plural = 'Accounts'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False, related_name='account')
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    CURRENCY_CHOICES = (
+        ('gdp','GBP'),
+        ('usd', 'USD'),
+        ('eur', 'EUR'),
+    )
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default='gdp')
+    created_at = models.DateTimeField(auto_now_add=True) #
+    STATUS_CHOICES = (
+        ('active', 'Active'),
+        ('inactive', 'Inactive'),
+        ('suspended', 'Suspended'),
+    )
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
+
+    def __str__(self):
+        """
+        Returns the username of the user linked to the account.
+
+        :return: str: The username of the user linked to the account
+        """
+        return self.user.username
+
+    def change_balance(self, amount):
+        """
+    Modifies the account balance by a specified amount. The amount can be positive (for deposits)
+    or negative (for withdrawals).
+
+    :param amount: The amount to adjust the balance by. Can be positive or negative.
+    :type amount: Decimal
+
+    :return: The updated account balance after applying the change.
+    :rtype: Decimal
+    """
+        self.balance += amount
+        self.save()
+        return self.balance
