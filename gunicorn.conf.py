36e760b20e8af1f483e314510414773ee52b0e21
Ubuntu <ubuntu@ip-172-31-31-233.ec2.internal>
2024-04-18 19:57:18 +0000

    update gunicorn.conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,3 +2,7 @@ def post_fork(server, worker):
     from thrift_timestamp.server import ThriftServerSingleton
     server = ThriftServerSingleton()
     server.start_server()
+
+certfile = '/home/ubuntu//webapps2024/webapps.crt'
+keyfile = '/home/ubuntu/webapps2024/webapps.pem'
+bind = '0.0.0.0:443'
