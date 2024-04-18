6c3244d6581ba3ff0d29c7f5940b23042b46cd09
Tom Naccarato <108072398+tnaccarato@users.noreply.github.com>
2024-04-18 16:11:33 +0100

    Refactor start thrift server to manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+from thrift_timestamp.server import ThriftServerSingleton
 
 
 def main():
@@ -9,14 +8,20 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapps2024.settings')
     try:
         from django.core.management import execute_from_command_line
+        from threading import Thread
+        from thrift_timestamp import server
+        thrift_server = ThriftServerSingleton()
+        thrift_server_thread = Thread(target=thrift_server.start_server)
+        thrift_server_thread.daemon = True
+        thrift_server_thread.start()
+
+        execute_from_command_line(sys.argv)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
--- register/apps.py
+++ register/apps.py
@@ -1,13 +1,6 @@
-from threading import Thread, Lock
-import threading
-
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 from register import create_admin_account
-from thrift_timestamp import server
-
-# Global lock for Thrift server start
-thrift_server_lock = Lock()
 
 
 class RegisterConfig(AppConfig):
@@ -15,16 +8,5 @@ class RegisterConfig(AppConfig):
     name = 'register'
 
     def ready(self):
-        # Check and start Thrift server in a new thread if not already running
-        def start_server():
-            with thrift_server_lock:
-                if not hasattr(threading.current_thread(), 'thrift_server_started'):
-                    server.start_thrift_server()
-                    setattr(threading.current_thread(), 'thrift_server_started', True)
-
-        thrift_server_thread = Thread(target=start_server)
-        thrift_server_thread.daemon = True
-        thrift_server_thread.start()
-
         # Connect the post_migrate signal
         post_migrate.connect(create_admin_account.create_admin_group_and_account, sender=self)
